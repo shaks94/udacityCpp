d34e4b84f35e03934007df4a04a2446330cff3fe
shubham sharma <shubham4516@gmail.com>
2018-03-17 12:02:45 +0530

    Initial Commit

---
+++ udacityCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  udacityCpp
+//
+//  Created by Admin on 17/03/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
